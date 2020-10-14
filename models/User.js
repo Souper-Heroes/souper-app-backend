2c2fdb78cc65af664f578251f74be8bcc8f32c4a
Your Correct Name <andystyles30@gmail.com>
2020-10-14 16:20:04 +0100

    updates to user and items schema

--- models/User.js
+++ models/User.js
@@ -4,14 +4,36 @@ const UserSchema = mongoose.Schema({
   _id: {
     type: String
   },
+  display_name: {
+    type: String,
+    required: true
+  },
   postcode: {
     type: String,
     required: false
+  },
+  address: {
+    type: String,
+    required: false
+  },
+  location: {
+    type: { type: String },
+    coordinates: []
+  },
+  preferred_distance_unit: {
+    type: { type: String },
+    coordinates: []
+  },
+  preferred_distance: {
+    type: { type: Number },
+    coordinates: []
   },
   date: {
     type: Date,
     default: Date.now
   }
 });
+
+UserSchema.index({ location: '2dsphere' });
 
 export default mongoose.model('user', UserSchema);
